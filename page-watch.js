dd8a2ed9ac2ebccbdc73733f8e2546d2fbbd0f71
Finn Smith <finnbarsmith@gmail.com>
2025-02-15 13:31:42 -0800

    Remove Twitter references in sendStatus because we aren't using Twitter anymore

--- page-watch.js
+++ page-watch.js
@@ -139,40 +139,6 @@ async function sendStatus(account, status, edit) {
       )
     }
 
-    // Twitter
-    if (account.access_token) {
-      const twitter = new Twit(account)
-      const b64content = fs.readFileSync(screenshot, {encoding: 'base64'})
-
-      // upload the screenshot to twitter
-      twitter.post('media/upload', {media_data: b64content}, function(err, data, response) {
-        if (err) {
-          console.log(err)
-          return
-        }
-
-        // add alt text for the media, for use by screen readers
-        const mediaIdStr = data.media_id_string
-        const altText = "Screenshot of edit to " + edit.page
-        const metaParams = {media_id: mediaIdStr, alt_text: {text: altText}}
-
-        twitter.post('media/metadata/create', metaParams, function(err, data, response) {
-          if (err) {
-            console.log('metadata upload for twitter screenshot alt text failed with error', err)
-          }
-          const params = {
-            'status': status,
-            'media_ids': [mediaIdStr]
-          }
-          twitter.post('statuses/update', params, function(err) {
-            if (err) {
-              console.log(err)
-            }
-          })
-        })
-      })
-    }
-
     // screenshot no longer needed
     fs.unlinkSync(screenshot)
   }
