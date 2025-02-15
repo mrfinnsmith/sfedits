ce23f5ecfc16c29ba8433b97233d642d22f8a379
Finn Smith <finnbarsmith@gmail.com>
2025-02-15 13:28:46 -0800

    Remove cantTweet because we are not using Twitter anymore

--- page-watch.js
+++ page-watch.js
@@ -196,29 +196,6 @@ function checkConfig(config, error) {
   }
 }
 
-function canTweet(account, error) {
-  if (!account.access_token) {
-    error(null)
-  } else {
-    try {
-      const twitter = new Twit(account)
-      const a = account['access_token']
-      return twitter.get('search/tweets', {q: 'cats'}, function(err, data, response) {
-        if (err) {
-          error(err + " for access_token " + a)
-        } else if (!response.headers['x-access-level'] ||
-            (response.headers['x-access-level'].substring(0,10) !== 'read-write')) {
-          error(`no read-write permission for access token ${a}`)
-        } else {
-          error(null)
-        }
-      })
-    } catch (err) {
-      error(`unable to create twitter client for account: ${account}`)
-    }
-  }
-}
-
 function main() {
   const config = getConfig(argv.config)
   return checkConfig(config, function(err) {
